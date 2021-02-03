7611f6cde81e1e8a48e306a207dff54dcc45761e
Josh Brand <jbrand@hashicorp.com>
2021-02-03 16:40:53 -0500

    Update comment on license secrets
    Signed-off-by: Josh Brand <jbrand@hashicorp.com>

--- modules/enos_infra/aws/kms.tf
+++ modules/enos_infra/aws/kms.tf
@@ -8,7 +8,7 @@ resource "aws_kms_alias" "enos_key_alias" {
   target_key_id = aws_kms_key.enos_key.key_id
 }
 
-# Stubbed out, we should store these someplace durable
+# Replace these with base64encode(file(something))
 resource "aws_kms_ciphertext" "enos_vault_license" {
   key_id    = aws_kms_key.enos_key.key_id
   plaintext = "this is a fake license"

5ba14dca09ecfa45cf8cb2462c7138d788b4a463
Josh Brand <jbrand@hashicorp.com>
2021-02-16 16:06:40 -0500

    Use subnet ids not cirs for instances, cleanup
    Signed-off-by: Josh Brand <jbrand@hashicorp.com>

--- modules/vault-cluster/aws/infra.tf
+++ modules/vault-cluster/aws/infra.tf
@@ -5,8 +5,3 @@ data "aws_vpc" "infra" {
 data "aws_subnet_ids" "infra" {
   vpc_id = var.vpc_id
 }
-
-data "aws_subnet" "infra" {
-  for_each = data.aws_subnet_ids.infra.ids
-  id       = each.value
-}
--- modules/vault-cluster/aws/vault-instances.tf
+++ modules/vault-cluster/aws/vault-instances.tf
@@ -1,13 +1,9 @@
-locals {
-  infra_subnet_blocks = [for s in data.aws_subnet.infra : s.cidr_block]
-}
-
 resource "aws_instance" "vault_instance" {
   count                  = var.instance_count
   ami                    = var.ubuntu_ami_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.enos_vault_sg.id]
-  subnet_id              = local.infra_subnet_blocks[count.index]
+  subnet_id              = tolist(data.aws_subnet_ids.infra.ids)[count.index]
   key_name               = var.ssh_aws_keypair
   tags = merge(
     var.common_tags,

bc10c81fe87e48196255bb902473092857f57192
Ryan Cragun <me@ryan.ec>
2022-10-12 16:26:23 -0600

    [QT-335] Update semver library for semverconstraint() (#73)
    This change fixes handling of constraints that don't include ranges that
    explicitly specify allowing prereleases.
    
    * Update the semver lib to v3
    * Add a test for constraints that previously were not handled correctly
    
    Signed-off-by: Ryan Cragun <me@ryan.ec>

---
+++ internal/flightplan/funcs/semver_test.go
@@ -0,0 +1,38 @@
+package funcs
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/zclconf/go-cty/cty"
+)
+
+func TestServerConstraint(t *testing.T) {
+	t.Parallel()
+
+	for desc, test := range map[string]struct {
+		version    string
+		constraint string
+		expected   bool
+	}{
+		"doesn't adhere to prerelases without a constraint range": {
+			"v1.13.0-dev1",
+			">=1.11.0",
+			false,
+		},
+		"respects prerelases with constraint range": {
+			"v1.13.0-dev1",
+			">=1.11.0-0",
+			true,
+		},
+	} {
+		t.Run(desc, func(t *testing.T) {
+			val, err := SemverConstraint.Call([]cty.Value{
+				cty.StringVal(test.version), cty.StringVal(test.constraint),
+			})
+
+			require.NoError(t, err)
+			require.Equal(t, cty.True, val.Equals(cty.BoolVal(test.expected)))
+		})
+	}
+}

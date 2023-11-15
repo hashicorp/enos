a4328b6ca27161d00d98e9aa58087e5eb26bdf45
Ryan Cragun <me@ryan.ec>
2023-11-15 13:46:15 -0700

    Bump deps and remove checks workaround (#117)
    Signed-off-by: Ryan Cragun <me@ryan.ec>

--- acceptance/scenarios/scenario_generate_pass_0/modules/bar/main.tf
+++ acceptance/scenarios/scenario_generate_pass_0/modules/bar/main.tf
@@ -14,4 +14,10 @@ variable "input" {
 variable "anotherinput" {
   type    = string
   default = "notset"
+
+  # Add a validation to ensure https://github.com/hashicorp/terraform-json/issues/106 is fixed.
+  validation {
+    condition     = length(var.anotherinput) > 0
+    error_message = "ensure checks in state are valid"
+  }
 }

238dcbee16c1b2331e3440c66b697e6e3c11516d
Ryan Cragun <me@ryan.ec>
2024-04-09 12:03:16 -0600

    ui.html: add a test for nil panics
    Signed-off-by: Ryan Cragun <me@ryan.ec>

---
+++ internal/ui/html/html_test.go
@@ -0,0 +1,30 @@
+package html
+
+import (
+	"testing"
+
+	"github.com/hashicorp/enos/proto/hashicorp/enos/v1/pb"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TestShowScenarioOutline tests showing the scenario outline.
+func TestShowScenarioOutline(t *testing.T) {
+	t.Parallel()
+
+	for name, test := range map[string]struct {
+		v func() *View
+	}{
+		"raw does not panic": {
+			func() *View { v := &View{}; return v },
+		},
+		"contructor does not panic": {
+			func() *View { v, err := New(); require.NoError(t, err); return v },
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			require.NoError(t, test.v().ShowScenarioOutline(&pb.OutlineScenariosResponse{}))
+		})
+	}
+}

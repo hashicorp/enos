e4e7c4de5f6954b34232a32c0ec1ecd587877ebd
Ryan Cragun <me@ryan.ec>
2024-04-10 10:40:37 -0600

    [QT-658] Update the homebrew workflow for the public tap
    Now that the enos bits are public we can use the public homebrew tap.
    This updates the template, formula, and publishing location. I also
    updated the tests to use a version of the formula that I've tested
    locally and know works.
    
    Signed-off-by: Ryan Cragun <me@ryan.ec>

--- tools/homebrew/main_test.go
+++ tools/homebrew/main_test.go
@@ -17,19 +17,19 @@ import (
 func Test_decodeMetadata(t *testing.T) {
 	t.Parallel()
 
-	p, err := filepath.Abs("./support/enos_0.0.1_SHA256SUMS")
+	p, err := filepath.Abs("./support/enos_0.0.27_SHA256SUMS")
 	require.NoError(t, err)
 
 	sums, err := readMetadata(p)
 	require.NoError(t, err)
 
 	expected := &metadata{
-		DarwinAMD64SHA: "788eda2be1887fa13b2aca2a5bcad4535278310946c8f6f68fa561e72f7a351b",
-		DarwinARM64SHA: "15d82aa03f5585966bc747e04ee31c025391dc1c80b3ba6419c95f6b764eebbd",
-		LinuxAMD64SHA:  "dc1f9597b024b59bf444d894c633c3fe796b7d57d74d7983ad57c4d7d37a516d",
-		LinuxARM64SHA:  "2a1677b83d6ec24038ef949420afa79269a405e35396aefec432412233cfc251",
-		Version:        "0.0.1",
-		VersionTag:     "v0.0.1",
+		DarwinARM64SHA: "e10ffb026f933eef8d40b25dc864557b473da344346c5c6fd54ae28062dc4932",
+		DarwinAMD64SHA: "155dc543097509fb7657867c468cc0a32ea62553eadfec34326f4633a577a043",
+		LinuxARM64SHA:  "81ed9db7983b9dc5e78cb40607afb8227a002b714113c0768bcbbcfcf339f941",
+		LinuxAMD64SHA:  "c4d4ae0d4de8315d18081a6719a634bbe263753e1f9d4a576c0b4650c2137af8",
+		Version:        "0.0.27",
+		VersionTag:     "v0.0.27",
 	}
 	require.Equal(t, expected, sums)
 }
@@ -38,7 +38,7 @@ func Test_decodeMetadata(t *testing.T) {
 func Test_renderHomebrewFormulaTemplate(t *testing.T) {
 	t.Parallel()
 
-	p, err := filepath.Abs("./support/enos_0.0.1_SHA256SUMS")
+	p, err := filepath.Abs("./support/enos_0.0.27_SHA256SUMS")
 	require.NoError(t, err)
 
 	buf := bytes.Buffer{}
--- tools/homebrew/support/enos.rb
+++ tools/homebrew/support/enos.rb
@@ -1,46 +1,35 @@
-require_relative "../Strategies/private_strategy"
 class Enos < Formula
-  desc "A tool for powering Software Quality as Code by writing Terraform-based quality requirement scenarios using a composable and shareable declarative language."
+  desc "A tool for powering Software Quality as Code by writing Terraform-based quality requirement scenarios using a composable, modular, and declarative language."
   homepage "https://github.com/hashicorp/enos"
-  version "0.0.1"
+  version "0.0.27"
 
   depends_on "hashicorp/tap/terraform"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/hashicorp/enos/releases/download/v0.0.1/enos_0.0.1_darwin_arm64.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "15d82aa03f5585966bc747e04ee31c025391dc1c80b3ba6419c95f6b764eebbd"
-
-      def install
-        bin.install "enos"
-      end
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/hashicorp/enos/releases/download/v0.0.1/enos_0.0.1_darwin_amd64.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "788eda2be1887fa13b2aca2a5bcad4535278310946c8f6f68fa561e72f7a351b"
-
-      def install
-        bin.install "enos"
-      end
-    end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/hashicorp/enos/releases/download/v0.0.27/enos_0.0.27_darwin_arm64.zip"
+    sha256 "e10ffb026f933eef8d40b25dc864557b473da344346c5c6fd54ae28062dc4932"
   end
 
-  on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/hashicorp/enos/releases/download/v0.0.1/enos_0.0.1_linux_arm64.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "2a1677b83d6ec24038ef949420afa79269a405e35396aefec432412233cfc251"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/hashicorp/enos/releases/download/v0.0.27/enos_0.0.27_darwin_amd64.zip"
+    sha256 "155dc543097509fb7657867c468cc0a32ea62553eadfec34326f4633a577a043"
+  end
 
-      def install
-        bin.install "enos"
-      end
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/hashicorp/enos/releases/download/v0.0.1/enos_0.0.1_linux_amd64.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "dc1f9597b024b59bf444d894c633c3fe796b7d57d74d7983ad57c4d7d37a516d"
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/hashicorp/enos/releases/download/v0.0.27/enos_0.0.27_linux_arm64.zip"
+    sha256 "81ed9db7983b9dc5e78cb40607afb8227a002b714113c0768bcbbcfcf339f941"
+  end
 
-      def install
-        bin.install "enos"
-      end
-    end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/hashicorp/enos/releases/download/v0.0.27/enos_0.0.27_linux_amd64.zip"
+    sha256 "c4d4ae0d4de8315d18081a6719a634bbe263753e1f9d4a576c0b4650c2137af8"
+  end
+
+  def install
+    bin.install "enos"
+  end
+
+  test do
+    system "#{bin}/enos --version"
   end
 end

ab7eb2f4b11313b334dceaeb013692f2807f153d
Ryan Cragun <me@ryan.ec>
2025-10-30 16:26:31 -0600

    fix linter
    Signed-off-by: Ryan Cragun <me@ryan.ec>

--- internal/flightplan/sample_subset_test.go
+++ internal/flightplan/sample_subset_test.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -266,11 +267,12 @@ sample "foo" {
 			for i := range test.expected {
 				sub := samp.Subsets[i]
 				frame, decRes := sub.Frame(context.Background(), test.ws)
-				msg := "expected an equal frame"
+				msg := strings.Builder{}
+				msg.WriteString("expected an equal frame")
 				for _, d := range decRes.GetDiagnostics() {
-					msg += " " + diagnostics.String(d)
+					msg.WriteString(" " + diagnostics.String(d))
 				}
-				require.Emptyf(t, decRes.GetDiagnostics(), msg)
+				require.Emptyf(t, decRes.GetDiagnostics(), msg.String())
 
 				testRequireEqualSampleSubsetFrame(t, test.expected[i], frame)
 			}
